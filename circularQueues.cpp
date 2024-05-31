bc375f4a7280c89df108058a3af4fd8445b54d13
Naufal0067 <naufalopang1928@gmail.com>
2024-05-31 15:01:41 +0700

    while iterasi dari awal array hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -78,6 +78,15 @@ public:
                 cout << queue_array[FRONT_position] << "  ";
                 FRONT_position++;
             }
+
+            FRONT_position = 0;
+
+            //Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
-}
+};
